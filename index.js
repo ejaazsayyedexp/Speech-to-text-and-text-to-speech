fc4a2429834b03c1c5a4478ff262fa4eff57df7a
ejaazsayyedexp <49115875+ejaazsayyedexp@users.noreply.github.com>
2019-08-16 22:29:29 +0530

    A node.js implemented simple javascript sppech to text and text to speech implementation
    This is an basic implementation of speech to text and text to speech in javascript. They are to be hosted by a node.js server, where they can be easily routed

---
+++ index.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+var app = express();
+app.use(bodyParser.urlencoded());
+//app.use(express.static('./view'));
+
+
+app.get('/tts',(req,res)=>{
+    res.sendFile('tts.html',{root:path.join(__dirname,'/view')});
+});
+app.get('/stt',(req,res)=>{
+    res.sendFile('main.html',{root:path.join(__dirname,"/view")})
+})
+
+
+app.listen(9000,()=>{
+    console.log("Server started at 9000");
+});
